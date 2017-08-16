478f4ca7b6b2d0bf0b8bddebeb08d0a1d385408e
Tocknicsu <wingemerald@gmail.com>
2017-08-16 15:47:19 +0800

    update

--- src/index.js
+++ src/index.js
@@ -2,27 +2,13 @@ import { GraphQLScalarType } from 'graphql';
 import { Kind } from 'graphql/language';
 
 function identity(value) {
-  return value;
+  return JSON.stringify(value);
 }
 
 function parseLiteral(ast) {
   switch (ast.kind) {
     case Kind.STRING:
-    case Kind.BOOLEAN:
-      return ast.value;
-    case Kind.INT:
-    case Kind.FLOAT:
-      return parseFloat(ast.value);
-    case Kind.OBJECT: {
-      const value = Object.create(null);
-      ast.fields.forEach((field) => {
-        value[field.name.value] = parseLiteral(field.value);
-      });
-
-      return value;
-    }
-    case Kind.LIST:
-      return ast.values.map(parseLiteral);
+      return JSON.parse(ast);
     default:
       return null;
   }
@@ -33,7 +19,7 @@ export default new GraphQLScalarType({
   description:
     'The `JSON` scalar type represents JSON values as specified by ' +
     '[ECMA-404](http://www.ecma-international.org/' +
-    'publications/files/ECMA-ST/ECMA-404.pdf).',
+    'publications/files/ECMA-ST/ECMA-404.pdf).(Note: You should stringify your json object.)',
   serialize: identity,
   parseValue: identity,
   parseLiteral,
